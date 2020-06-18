ca539570cb85833f850c01fdc18fba962826c80a
Alina Tylets <atylets@slb.com>
2020-06-18 18:06:21 +0300

    feat: add basic components

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-servers',
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.scss'],
+})
+export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus = 'No sever was created!';
+    serverName = 'Test server';
+    serverCreated = false;
+    servers = ['Test Server 1', 'Test Server 2'];
+
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
+    }
+
+    ngOnInit() {}
+
+    onCreateServer() {
+        this.serverCreated = true;
+        this.servers.push(this.serverName);
+        this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
+    }
+}
